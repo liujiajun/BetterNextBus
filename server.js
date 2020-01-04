d66bc120790cefe95143fd7f0db1320ae822de97
liujiajun <liujiajun1999@gmail.com>
2020-01-04 22:03:11 +0800

    Bug fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-let app = express();
+app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);

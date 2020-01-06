3f6c9dc1f8a3a8f9fcab7b4cf49169e4021e356d
liujiajun <liujiajun1999@gmail.com>
2020-01-06 19:35:05 +0800

    Add animation

--- src/components/FavoriteList.vue
+++ src/components/FavoriteList.vue
@@ -1,14 +1,17 @@
 <template>
     <div id="favorite-list">
         <v-container class="pt-0 mt-0">
-            <v-row class="pt-0 mt-0">
-                <v-col
-                        v-for="name in $store.state.favorites"
-                        :key="name"
-                        cols="12">
-                   <favorite-card :stop_name="name"></favorite-card>
-                </v-col>
-            </v-row>
+                <v-slide-y-transition
+                        group
+                        tag="v-row"
+                >
+                    <v-col
+                            v-for="name in $store.state.favorites"
+                            :key="name"
+                            cols="12">
+                        <favorite-card :stop_name="name"></favorite-card>
+                    </v-col>
+                </v-slide-y-transition>
         </v-container>
     </div>
 </template>

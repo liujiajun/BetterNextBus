7603de95144161b385106557869879b94d3f4df2
liujiajun <liujiajun1999@gmail.com>
2020-01-14 10:28:55 +0800

    Add Google analytics

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import store from "./store";
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = true;
 Vue.prototype.$hostname = (Vue.config.productionTip) ? "https://better-nextbus.appspot.com/" : "http://127.0.0.1:5000/";
 
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+    id: "UA-155726050-1",
+    router
+});
 
 new Vue({
     vuetify,

9a89bccd26691f4f8207790133c420f7a8005d44
Chen Yongyan <34476762+Raymond0212@users.noreply.github.com>
2020-01-16 16:36:31 +0800

    Update Facebook icon and link (#5)

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
 export const GITHUB_URL = "https://github.com/liujiajun/BetterNextBus";
 export const EMAIL_ENCODED = "bGl1amlhanVuMTk5OUBnbWFpbC5jb20=";
+export const FACEBOOK_URL = "https://www.facebook.com/BetterNextBus/";

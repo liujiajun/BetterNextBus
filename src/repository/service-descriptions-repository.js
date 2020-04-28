7e81f4cf3fa5648a0283a531d58e172030970c20
liujiajun <liujiajun1999@gmail.com>
2020-04-28 23:11:29 +0800

    Static bus info

--- src/repository/service-descriptions-repository.js
+++ src/repository/service-descriptions-repository.js
@@ -1,21 +1,12 @@
-import repository from "./repository";
+//import repository from "./repository";
+import busInfo from "../data/bus-info";
 
-// const RESOURCE = "/ServiceDescription";
-const RESOURCE = "/BusInfo";
+//const RESOURCE = "/BusInfo";
 
 export default {
     async get() {
-        let res = await repository.get(RESOURCE);
-        // let routes = [];
-        // res.data["ServiceDescriptionResult"]["ServiceDescription"].forEach(route => {
-        //     routes.push({
-        //         service_name: route["Route"],
-        //         description: route["RouteDescription"],
-        //         check_points: [],
-        //         pickup_points: []
-        //     });
-        // });
-        // return routes;
-        return res.data;
+        //let res = await repository.get(RESOURCE);
+        //return res.data;
+        return busInfo;
     }
 };
